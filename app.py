19a66362e4bd1317c5426b32115a4e71743bb2d4
Mutahira213 <mutahiraalam7@gmail.com>
2026-05-20 19:38:19 +0500

    Add: auto-open browser on app start

--- app.py
+++ app.py
@@ -170,4 +170,6 @@ def reset():
 # ---------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+   import webbrowser
+webbrowser.open("http://127.0.0.1:5000")
+app.run(debug=True, host="0.0.0.0", port=5000)
